bbe88ce6faaab1f486a4a753bff3888d6617ab1b
johnnybhojwani <50454193+johnnybhojwani@users.noreply.github.com>
2023-05-03 18:26:58 -0500

    Update time_arrays.py

--- time_arrays.py
+++ time_arrays.py
@@ -1,3 +1,11 @@
+'''
+This script compares the time taken to do the same task using a Python list and a Numpy Array.
+In both cases, the program creates a data structure that holds an array of 10,000,000 random
+numbers between 1 and 7. Each process is clocked to evaluate the time taken. As the script 
+will prove, Numpy arrays can run up to 100 times faster than ordinary Python list objects.
+'''
+
+
 import time
 import numpy as np
 import random
@@ -6,12 +14,12 @@ start_time_list = time.time()
 
 list1 = [random.randrange(1,7) for i in range(0,10_000_000)]
 
-print(f"Process finished --- {(time.time() - start_time_list)} seconds ---")
+print(f"Python List --- Process finished --- {(time.time() - start_time_list)} seconds ---")
 
 
 start_time_array = time.time()
 
 array1 = np.random.randint(1, 7, 10_000_000)
 
-print(f"Process finished --- {(time.time() - start_time_array)} seconds ---")
+print(f"Numpy Array --- Process finished --- {(time.time() - start_time_array)} seconds ---")
 
